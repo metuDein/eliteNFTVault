f23f7f09b1934312f85ad58a682b121e633db311
metu dein <maxmetudein.com>
2025-01-19 15:22:24 +0100

    update layout v2

--- config/index.jsx
+++ config/index.jsx
@@ -2,25 +2,23 @@ import { cookieStorage, createStorage, http } from "@wagmi/core";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import { mainnet, arbitrum } from "@reown/appkit/networks";
 
-// Get projectId from environment variables
+// Get projectId from https://cloud.reown.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 
 if (!projectId) {
   throw new Error("Project ID is not defined");
 }
 
-// Define the supported networks
 export const networks = [mainnet, arbitrum];
 
-// Set up the Wagmi Adapter (Config)
+//Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
   storage: createStorage({
-    storage: typeof window === "undefined" ? undefined : cookieStorage, // Ensure storage is accessible in SSR
+    storage: cookieStorage,
   }),
   ssr: true,
   projectId,
   networks,
 });
 
-// Export the adapter's wagmiConfig for use in the app
 export const config = wagmiAdapter.wagmiConfig;
